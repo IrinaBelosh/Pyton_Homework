d25334763f9bab04a690dc4988c37224929cd58e
Irina <Aniri_Varga@mail.ru>
2023-02-02 15:45:39 +0300

    Task_4 done

--- Homework_1/Task_4.py
+++ Homework_1/Task_4.py
@@ -1,7 +1,19 @@
-# 4. Напишите программу, которая по заданному номеру четверти, 
+# 4. Напишите программу, которая по заданному номеру четверти,
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 # *Пример:*
 
 # - 1 -> x > 0, y > 0
 # - 8 -> нет такой четверти
+
+quarter = int(input('Enter the number of a quarter: '))
+if quarter == 1:
+    print('x > 0 and y > 0')
+elif quarter == 2:
+    print('x < 0 and y > 0')
+elif quarter == 3:
+    print('x < 0 and y < 0')
+elif quarter == 4:
+    print('x > 0 and y < 0')
+else:
+    print('There is not such quarter. Enter other number')
 
